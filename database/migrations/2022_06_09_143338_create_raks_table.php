2ae6c5840fe82b279f10477122f8f28425d551c0
Muhammad Farhan Al Rahman <muhammadfarchanalrahman5994@gmail.com>
2022-12-20 20:28:42 +0700

    add reset password

--- database/migrations/2022_06_09_143338_create_raks_table.php
+++ database/migrations/2022_06_09_143338_create_raks_table.php
@@ -15,6 +15,7 @@ return new class extends Migration {
         Schema::create('raks', function (Blueprint $table) {
             $table->id();
             $table->string('kategori');
+            $table->string('nama_gambar');
             $table->timestamps();
         });
     }
