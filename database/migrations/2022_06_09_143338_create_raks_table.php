10db3b6026d1ebae41bdf0ed27fc9bae0d4824bf
Muhammad Farhan Al Rahman <muhammadfarchanalrahman5994@gmail.com>
2022-12-28 22:49:26 +0700

    add foto raks migration

--- database/migrations/2022_06_09_143338_create_raks_table.php
+++ database/migrations/2022_06_09_143338_create_raks_table.php
@@ -16,7 +16,6 @@ return new class extends Migration {
             $table->id();
             $table->string('kategori');
             $table->string('nama_gambar');
-            $table->string('foto') ;
             $table->timestamps();
         });
     }
